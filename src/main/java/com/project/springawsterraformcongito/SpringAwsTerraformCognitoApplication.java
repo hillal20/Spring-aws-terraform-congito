2e93eb9cd65301c251c25067531796a5b724c4d2
AITC (Hilal) <haissani@flexion.us>
2023-11-11 14:59:40 -0500

    Task-01: initiatlizing my application

---
+++ src/main/java/com/project/springawsterraformcongito/SpringAwsTerraformCognitoApplication.java
@@ -0,0 +1,14 @@
+package com.project.springawsterraformcongito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsTerraformCognitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsTerraformCognitoApplication.class, args);
+        System.out.println("**** Sprig boot application started ****");
+    }
+
+}
---
+++ src/test/java/com/project/springawsterraformcongito/SpringAwsTerraformCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springawsterraformcongito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsTerraformCognitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
